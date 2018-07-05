4ae84c32675114505b3ff85eaf6ecda11dad3bb0
virtuesun <softsmile@126.com>
2018-07-05 12:59:07 +0800

    create learngit project.

---
+++ learngit/src/main/java/cn/softsmile/learngit/LearngitApplication.java
@@ -0,0 +1,12 @@
+package cn.softsmile.learngit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearngitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearngitApplication.class, args);
+    }
+}
---
+++ learngit/src/main/java/cn/softsmile/learngit/controller/HelloController.java
@@ -0,0 +1,17 @@
+package cn.softsmile.learngit.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author lideyang
+ * @date 2018/7/5
+ */
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String hello(){
+        return "hello git.";
+    }
+}
